8177c5bb1974b035ff84971987a42231b7e17e9f
Ahdab Serhan <ahdabs@amdocs.com>
2020-10-13 14:41:35 +0300

    intagram flutter App

---
+++ Instagram/lib/pages/notifications.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:Instagram/models/post.dart';
+import 'package:Instagram/models/user.dart';
+import 'package:Instagram/models/comment.dart';
+import 'package:Instagram/models/global.dart';
+
+class NotificationsPage extends StatefulWidget {
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
